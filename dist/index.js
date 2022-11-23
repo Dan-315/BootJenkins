87adfb69c4bc494fe5587547825f1e10e973e1df
Dan315 <daniel827874735@gmail.com>
2022-11-23 16:47:02 -0600

    Creacion del proyecto

---
+++ dist/index.js
@@ -0,0 +1,108 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+// let host="https://apprende-ws.herokuapp.com/graphql"
+let host = "http://localhost:3000/graphql";
+let values = [null, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, "Daniel", "David ", 3];
+let numPeticiones = 10;
+let query = `query($user: UserInput){
+    getUser(user: $user) {
+      id,
+      nombre,
+      apePat,
+      apeMat,
+      telefono,
+      email,
+      pasword
+    }  
+  }`;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function init() {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log(`
+        ////////////////////////////////////////////////////////////////////////////////////////////
+            Iniciando peticiones a ${host}
+        ////////////////////////////////////////////////////////////////////////////////////////////
+    `);
+        let PeticionesExitosas = 0;
+        let PeticionesFracasadas = 0;
+        let ArrayErrores = [];
+        for (let i = 0; i < numPeticiones; i++) {
+            let user = {
+                id: getValue(),
+                nombre: getValue(),
+                apePat: getValue(),
+                apeMat: getValue(),
+                telefono: getValue(),
+                email: getValue(),
+                pasword: getValue()
+            };
+            // console.log(user);
+            yield peticion(user).then((res) => {
+                // console.log(res);
+                if (res.status) {
+                    PeticionesExitosas += 1;
+                }
+                else {
+                    PeticionesFracasadas += 1;
+                    ArrayErrores.push(res);
+                }
+            });
+        }
+        console.log("Errores");
+        console.log(ArrayErrores);
+        console.log(`
+        ----------------------------------------------------------------------------------------
+            Peticiones Exitosas: ${PeticionesExitosas}
+            Peticiones Fracasadas: ${PeticionesFracasadas}
+    `);
+    });
+}
+function getValue() {
+    let value = Math.trunc(Math.random() * (values.length - 0));
+    // console.log(value);
+    return values[value];
+}
+function peticion(user) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return yield axios_1.default.post(host, {
+            query,
+            variables: { user }
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((res) => {
+            let respons = {
+                status: true,
+                message: "OK",
+                data: res.data.data.getUser
+            };
+            return respons;
+        }).catch((error) => {
+            let respons = {
+                status: false,
+                message: error.message,
+                error: true,
+                errorData: error
+            };
+            return respons;
+        });
+    });
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+init();
---
+++ index.ts
@@ -0,0 +1,119 @@
+import axios from 'axios'
+
+
+// let host="https://apprende-ws.herokuapp.com/graphql"
+let host="http://localhost:3000/graphql"
+let values=[null,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,"Daniel","David ",3];
+let numPeticiones=10;
+
+let query=`query($user: UserInput){
+    getUser(user: $user) {
+      id,
+      nombre,
+      apePat,
+      apeMat,
+      telefono,
+      email,
+      pasword
+    }  
+  }`
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+interface Usuario{
+    id?: any,
+    nombre?: any,
+    apePat?: any,
+    apeMat?: any,
+    telefono?: any,
+    email?: any,
+    pasword?: any
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+async function init(){
+    console.log(`
+        ////////////////////////////////////////////////////////////////////////////////////////////
+            Iniciando peticiones a ${host}
+        ////////////////////////////////////////////////////////////////////////////////////////////
+    `);
+
+    let PeticionesExitosas=0;
+    let PeticionesFracasadas=0;
+    let ArrayErrores:any[]=[];
+
+    for(let i=0;i<numPeticiones;i++){
+    
+        let user:Usuario={
+            id:getValue(),
+            nombre:getValue(),
+            apePat:getValue(),
+            apeMat:getValue(),
+            telefono:getValue(),
+            email:getValue(),
+            pasword:getValue()
+        }
+        // console.log(user);
+        
+        await peticion(user).then((res)=>{
+            // console.log(res);
+            if(res.status){
+                PeticionesExitosas+=1;
+            }else{
+                PeticionesFracasadas+=1;
+                ArrayErrores.push(res);
+            }
+        })
+    }
+    console.log("Errores");
+    console.log(ArrayErrores);
+    
+    console.log(`
+        ----------------------------------------------------------------------------------------
+            Peticiones Exitosas: ${PeticionesExitosas}
+            Peticiones Fracasadas: ${PeticionesFracasadas}
+    `);
+}
+
+function getValue() {
+    let value=Math.trunc(Math.random() * (values.length - 0))
+    // console.log(value);
+    return values[value];
+}
+
+async function peticion(user:Usuario){
+    return await axios.post(host, {
+        query,
+        variables: {user}
+      },{
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then((res)=>{
+        let respons={
+          status:true,
+          message:"OK",
+          data:res.data.data.getUser
+        }
+        return respons;
+      }).catch((error:Error)=>{
+        let respons={
+          status:false,
+          message:error.message,
+          error:true,
+          errorData:error
+        }
+        return respons;
+      })
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+init();
+
+
